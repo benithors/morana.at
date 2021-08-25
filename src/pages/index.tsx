750deb6e30d28a575a0324f041c3df9c291e9ea6
Beni <thorstensen.benjamin@gmail.com>
2021-08-25 22:42:29 +0200

    responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,12 @@ import dynamic from 'next/dynamic'
 import {withUserAgent, WithUserAgentProps} from 'next-useragent'
 
 const DesktopContent = dynamic(() => import('../components/DesktopContent'))
-const MobileContent = dynamic(() => import('../components/MobileContent'))
 
 class Index extends React.Component<WithUserAgentProps> {
 
-    static async getInitialProps(ctx) {
-        return {useragent: ctx.ua.source}
-    }
+
 
     render() {
-        const {ua} = this.props
         return (
             <DesktopContent/>
         )
